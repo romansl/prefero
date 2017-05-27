3f626ac497255c1e1a5c4dd25ce5b6755ddeb672
Roman Leukin <leukinrs@gmail.com>
2017-05-27 09:53:14 +0300

    Refactor.

--- generator/src/main/java/com/romansl/prefs/Processor.kt
+++ generator/src/main/java/com/romansl/prefs/Processor.kt
@@ -25,75 +25,13 @@ class Processor : AbstractProcessor() {
             val kind = it.kind
             if (kind == ElementKind.INTERFACE) {
                 //val fullClassName = (it as QualifiedNameable).qualifiedName.toString()
-                val methods = it.getEnclosedElements().filter {
+                val properties = it.getEnclosedElements().filter {
                     it.kind == ElementKind.METHOD && it.simpleName.length > 3 && it.simpleName.startsWith("get")
+                }.map {
+                    Property(it as ExecutableElement)
                 }
-                val typeName = ClassName.get(it)
-                val className = ClassName.get(typeName.packageName(), "${typeName.simpleName()}Preferences")
-
-                val editorClass = TypeSpec.classBuilder("Editor")
-                        .addModifiers(KModifier.INNER)
-                        .addSuperinterface(typeName)
-                        .primaryConstructor(FunSpec.constructorBuilder()
-                                .addParameter("editor", spEditor)
-                                .build())
-                        .addProperty(PropertySpec.builder("editor", spEditor, KModifier.PRIVATE)
-                                .initializer("editor")
-                                .build())
-                        .apply {
-                            methods.forEach {
-                                it as ExecutableElement
-                                val propertyType = makePropertyType(it)
-                                val editorTypeName = makeEditorTypeName(propertyType, it)
-                                val name = makePropertyName(it)
-                                val keyName = makeKeyName(it, name)
-                                addProperty(PropertySpec.builder(name, propertyType, KModifier.OVERRIDE)
-                                        .mutable(true)
-                                        .getter(FunSpec.getterBuilder().addCode("return this@${className.simpleName()}.$name\n").build())
-                                        .setter(FunSpec.setterBuilder()
-                                                .addParameter("value", propertyType)
-                                                .addCode("editor.put$editorTypeName(%S, value)\n", keyName)
-                                                .build())
-                                        .build())
-                            }
-                        }
-                        .build()
-
-                val prefsClass = TypeSpec.classBuilder(className)
-                        .addSuperinterface(typeName)
-                        .primaryConstructor(FunSpec.constructorBuilder()
-                                .addParameter("preferences", spName)
-                                .build())
-                        .addProperty(PropertySpec.builder("pref", spName, KModifier.PRIVATE)
-                                .initializer("preferences")
-                                .build())
-                        .apply {
-                            methods.forEach {
-                                it as ExecutableElement
-                                val propertyType = makePropertyType(it)
-                                val editorTypeName = makeEditorTypeName(propertyType, it)
-                                val name = makePropertyName(it)
-                                val keyName = makeKeyName(it, name)
-                                val default = makeDefaultValue(it, propertyType)
-                                addProperty(PropertySpec.builder(name, propertyType, KModifier.OVERRIDE)
-                                        .getter(FunSpec.getterBuilder().addCode("return pref.get$editorTypeName(%S, $default)\n", keyName).build())
-                                        .build())
-                            }
-                        }
-                        .addType(editorClass)
-                        .addFun(FunSpec.builder("edit")
-                                .addParameter("body", EditorBodyType())
-                                .addCode("val editor = pref.edit()\n")
-                                .addCode("Editor(editor).body()\n")
-                                .addCode("editor.commit()\n")
-                                .build())
-                        .build()
-
                 if (!hasErrors) {
-                    KotlinFile.builder(className.packageName(), className.simpleName())
-                            .addType(prefsClass)
-                            .build()
-                            .writeTo(processingEnv.filer)
+                    generateFile(it, properties)
                 }
             } else {
                 error("The Preferences annotation can only be applied to interfaces.", it)
@@ -103,97 +41,61 @@ class Processor : AbstractProcessor() {
         return true
     }
 
-    private fun makeKeyName(element: ExecutableElement, name: String): String {
-        return element.getAnnotation(Key::class.java)?.name ?: name
-    }
+    private fun generateFile(it: TypeElement, properties: List<Property>) {
+        val typeName = ClassName.get(it)
+        val className = ClassName.get(typeName.packageName(), "${typeName.simpleName()}Preferences")
 
-    private fun makeDefaultValue(it: ExecutableElement, propertyType: TypeName): Any {
-        val defaultInt = it.getAnnotation(DefaultInt::class.java)
-        if (defaultInt != null) {
-            require(propertyType == INT) { "Int required" }
-            return defaultInt.value
-        }
+        val editorClass = TypeSpec.classBuilder("Editor")
+                .addModifiers(KModifier.INNER)
+                .addSuperinterface(typeName)
+                .primaryConstructor(FunSpec.constructorBuilder()
+                        .addParameter("editor", spEditor)
+                        .build())
+                .addProperty(PropertySpec.builder("editor", spEditor, KModifier.PRIVATE)
+                        .initializer("editor")
+                        .build())
+                .addProperties(properties.map {
+                    PropertySpec.builder(it.name, it.propertyType, KModifier.OVERRIDE)
+                            .mutable(true)
+                            .getter(FunSpec.getterBuilder()
+                                    .addCode("return this@${className.simpleName()}.${it.name}\n")
+                                    .build())
+                            .setter(FunSpec.setterBuilder()
+                                    .addParameter("value", it.propertyType)
+                                    .addCode("editor.put${it.editorTypeName}(%S, value)\n", it.keyName)
+                                    .build())
+                            .build()
+                })
+                .build()
 
-        val defaultLong = it.getAnnotation(DefaultLong::class.java)
-        if (defaultLong != null) {
-            require(propertyType == LONG) { "Long required" }
-            return "" + defaultLong.value + "L"
-        }
+        val prefsClass = TypeSpec.classBuilder(className)
+                .addSuperinterface(typeName)
+                .primaryConstructor(FunSpec.constructorBuilder()
+                        .addParameter("preferences", spName)
+                        .build())
+                .addProperty(PropertySpec.builder("pref", spName, KModifier.PRIVATE)
+                        .initializer("preferences")
+                        .build())
+                .addProperties(properties.map {
+                    PropertySpec.builder(it.name, it.propertyType, KModifier.OVERRIDE)
+                            .getter(FunSpec.getterBuilder()
+                                    .addCode("return pref.get${it.editorTypeName}(%S, ${it.default})\n", it.keyName)
+                                    .build())
+                            .build()
+                })
+                .addType(editorClass)
+                .addFun(FunSpec.builder("edit")
+                        .addParameter("body", EditorBodyType())
+                        .addCode("val editor = pref.edit()\n")
+                        .addCode("Editor(editor).body()\n")
+                        .addCode("editor.commit()\n")
+                        .build())
+                .build()
 
-        val defaultString = it.getAnnotation(DefaultString::class.java)
-        if (defaultString != null) {
-            require(propertyType == STRING) { "String required" }
-            return "\"" + defaultString.value + "\""
-        }
-
-        val defaultBool = it.getAnnotation(DefaultBoolean::class.java)
-        if (defaultBool != null) {
-            require(propertyType == BOOLEAN) { "Boolean required" }
-            return defaultBool.value
-        }
-
-        val defaultFloat = it.getAnnotation(DefaultFloat::class.java)
-        if (defaultFloat != null) {
-            require(propertyType == FLOAT) { "Float required" }
-            return "" + defaultFloat.value + "f"
-        }
-
-        return when (propertyType) {
-            INT -> "0"
-            LONG -> "0L"
-            STRING -> "\"\""
-            STRING_NULLABLE -> "null"
-            BOOLEAN -> "false"
-            FLOAT -> "0f"
-            else -> throw IllegalArgumentException("Illegal type " + propertyType.toString())
-        }
-    }
-
-    private fun makePropertyName(it: ExecutableElement): String {
-        val name = run {
-            val n = it.simpleName.toString()
-            val sb = StringBuilder(n.substring(3))
-            sb[0] = sb[0].toLowerCase()
-            sb.toString()
-        }
-        return name
-    }
-
-    private fun makeEditorTypeName(propertyType: TypeName, element: Element): String {
-        return when (propertyType.asNonNullable().toString()) {
-            "kotlin.Int" -> "Int"
-            "kotlin.Long" -> "Long"
-            "kotlin.Boolean" -> "Boolean"
-            "kotlin.Float" -> "Float"
-            "kotlin.String" -> "String"
-            else -> {
-                error("This property can not be Nullable.", element)
-                ""
-            }
-        }
-    }
-
-    private fun makePropertyType(element: ExecutableElement): TypeName {
-        val type = TypeName.get(element.returnType)
-        val propertyType = when (type.toString()) {
-            "java.lang.String" -> STRING
-            "java.lang.Integer" -> INT
-            "java.lang.Long" -> LONG
-            "java.lang.Float" -> FLOAT
-            "java.lang.Boolean" -> BOOLEAN
-            else -> type
-        }
-
-        return if (element.getAnnotation(Nullable::class.java) != null) {
-            if (propertyType == STRING) {
-                propertyType.asNullable()
-            } else {
-                error("This property can not be Nullable.", element)
-                propertyType
-            }
-        } else {
-            propertyType
-        }
+        KotlinFile.builder(className.packageName(), className.simpleName())
+                .addType(prefsClass)
+                .build()
+                .writeTo(processingEnv.filer)
     }
 
     private fun note(message: String) {
@@ -216,6 +118,107 @@ class Processor : AbstractProcessor() {
             } catch (ignored: Exception) {
             }
             throw e
+        }
+    }
+
+    inner class Property(it: ExecutableElement) {
+        val propertyType = makePropertyType(it)
+        val editorTypeName = makeEditorTypeName(propertyType, it)
+        val name = makePropertyName(it)
+        val keyName = makeKeyName(it, name)
+        val default = makeDefaultValue(it, propertyType)
+
+        private fun makeKeyName(element: ExecutableElement, name: String): String {
+            return element.getAnnotation(Key::class.java)?.name ?: name
+        }
+
+        private fun makeDefaultValue(it: ExecutableElement, propertyType: TypeName): Any {
+            val defaultInt = it.getAnnotation(DefaultInt::class.java)
+            if (defaultInt != null) {
+                require(propertyType == INT) { "Int required" }
+                return defaultInt.value
+            }
+
+            val defaultLong = it.getAnnotation(DefaultLong::class.java)
+            if (defaultLong != null) {
+                require(propertyType == LONG) { "Long required" }
+                return "" + defaultLong.value + "L"
+            }
+
+            val defaultString = it.getAnnotation(DefaultString::class.java)
+            if (defaultString != null) {
+                require(propertyType == STRING) { "String required" }
+                return "\"" + defaultString.value + "\""
+            }
+
+            val defaultBool = it.getAnnotation(DefaultBoolean::class.java)
+            if (defaultBool != null) {
+                require(propertyType == BOOLEAN) { "Boolean required" }
+                return defaultBool.value
+            }
+
+            val defaultFloat = it.getAnnotation(DefaultFloat::class.java)
+            if (defaultFloat != null) {
+                require(propertyType == FLOAT) { "Float required" }
+                return "" + defaultFloat.value + "f"
+            }
+
+            return when (propertyType) {
+                INT -> "0"
+                LONG -> "0L"
+                STRING -> "\"\""
+                STRING_NULLABLE -> "null"
+                BOOLEAN -> "false"
+                FLOAT -> "0f"
+                else -> throw IllegalArgumentException("Illegal type " + propertyType.toString())
+            }
+        }
+
+        private fun makePropertyName(it: ExecutableElement): String {
+            val name = run {
+                val n = it.simpleName.toString()
+                val sb = StringBuilder(n.substring(3))
+                sb[0] = sb[0].toLowerCase()
+                sb.toString()
+            }
+            return name
+        }
+
+        private fun makeEditorTypeName(propertyType: TypeName, element: Element): String {
+            return when (propertyType.asNonNullable().toString()) {
+                "kotlin.Int" -> "Int"
+                "kotlin.Long" -> "Long"
+                "kotlin.Boolean" -> "Boolean"
+                "kotlin.Float" -> "Float"
+                "kotlin.String" -> "String"
+                else -> {
+                    error("This property can not be Nullable.", element)
+                    ""
+                }
+            }
+        }
+
+        private fun makePropertyType(element: ExecutableElement): TypeName {
+            val type = TypeName.get(element.returnType)
+            val propertyType = when (type.toString()) {
+                "java.lang.String" -> STRING
+                "java.lang.Integer" -> INT
+                "java.lang.Long" -> LONG
+                "java.lang.Float" -> FLOAT
+                "java.lang.Boolean" -> BOOLEAN
+                else -> type
+            }
+
+            return if (element.getAnnotation(Nullable::class.java) != null) {
+                if (propertyType == STRING) {
+                    propertyType.asNullable()
+                } else {
+                    error("This property can not be Nullable.", element)
+                    propertyType
+                }
+            } else {
+                propertyType
+            }
         }
     }
 
